766a024fbf324202c60d8ec31fb54e094d281ed8
aafrinshahas <codergit6@gmail.com>
2024-04-17 21:15:39 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+      <nav>
+          <h1>Perfumy</h1>
+          <ul>
+              <li><a href="home.html">Home</a> | </li>
+              <li><a href="product.html">Products</a> | </li>
+              <li><a href="about.html">About</a> | </li>
+              <li><a href="contact.html">Contact</a> | </li>
+          </ul>
+          </nav>
+    )
+  }
+
+  export default Navbar;
